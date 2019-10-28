3b7efec75079e2eab7873d1cea6c16fa18849cc3
nyarbouh <nyarbouh@me.com>
2019-10-28 22:47:22 +0000

    Nizar test

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,6 +3,7 @@ import React from "react";
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
+		<h2> Hello world By Nizar </h2>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
