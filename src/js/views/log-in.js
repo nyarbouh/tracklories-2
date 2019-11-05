0074a0bf8a853c1a3a8b045540f1c4b7a3987d71
marcelvenegas <marcel.venega@gmail.com>
2019-11-05 15:22:24 +0000

    MI login ahi mas o menos!

--- src/js/views/log-in.js
+++ src/js/views/log-in.js
@@ -3,8 +3,29 @@ import React, { Component } from "react";
 export class Login extends Component {
 	state = {};
 	render() {
-		return <p>this is a test</p>;
+		return (
+			<div className="contanier-fluid">
+				<div className="row">
+					<div className="offset-md-4 col-md-4">
+						<h2>LOGIN</h2>
+
+						<form action="">
+							<div className="form-group">
+								<label htmlFor="">Username</label>
+								<input type="text" className="form-control" />
+							</div>
+
+							<div className="form-group">
+								<label htmlFor="">Password</label>
+								<input type="password" className="form-control" />
+							</div>
+							<button type="button" className="btn btn-outline-primary">
+								Login
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
-
-export default Login;
